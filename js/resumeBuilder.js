0aebe2a4a04b4e90f68ec32b1d63e59a5f7a3c22
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2017-07-08 15:50:43 -0500

    Prepend formattedRole and formattedName

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,9 +7,16 @@ This is empty on purpose! Your code to build the resume will go here.
  // console.log(awesomeThoughts);
  // console.log(funThoughts);
  // $("#main").append(funThoughts);
+
+// DATA VARIABLES
 var name = "Bobby Joe Smith III";
 var role = "Web Developer";
+
+// DATA REPLACEMENTS
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
+
+// Prepend name and role so they go before unordered list
+// Order matters
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
