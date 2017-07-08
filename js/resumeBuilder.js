c99db6fa87ce944d5f71814a94409c44fd77aa6f
BobbyJoeSmith3 <bobbyjoe@codeforprogress.org>
2017-07-08 15:38:02 -0500

    Populated data for headerName and headerRole, and appended to #header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ // $("#main").append("Bobby Joe Smith III");
+ // var awesomeThoughts = "I am Bobby Joe and I am AWESOME!";
+ // var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+ // console.log(awesomeThoughts);
+ // console.log(funThoughts);
+ // $("#main").append(funThoughts);
+var formattedName = HTMLheaderName.replace("%data%", "Bobby Joe Smith III");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
